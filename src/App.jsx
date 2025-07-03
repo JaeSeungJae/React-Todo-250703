138a761089c74550eb8e88a75d0659adf8b28297
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-07-03 11:42:55 +0900

    feat: 할일 리스트 추가 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  return <>Hello World</>
+  const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    const newTodo = e.target[0].value.trim()
+    if (newTodo) {
+      setTodos((prevTodos) => [newTodo, ...prevTodos])
+    }
+    e.target[0].value = ''
+  }
+  return (
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" name="todo" placeholder="" />
+        <button type="submit">등록</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default App
