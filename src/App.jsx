f913eeb4211db0987e3f2dcacc0c60beeeac4ea3
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-07-04 16:34:31 +0900

    fix: 주석 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-// import { useRef, useState } from 'react'
 import TodoWriteForm from './components/TodoWriteForm'
 import TodoList from './components/TodoList'
-// import useTodos from './hooks/useTodos'
 
 function App() {
-  //const { todos, handleOnSubmit, removeTodo, toggleTodo } = useTodos()
-
   return (
     <>
       <TodoWriteForm />
